46f88fad4752ad455bc0f955b43a5a511d11fbb2
Mateusz Borycki <mateuszborycki@gmail.com>
2025-04-07 06:22:20 +0200

    Update chat.spec.ts

--- test/chat.spec.ts
+++ test/chat.spec.ts
@@ -1,35 +1,91 @@
 import { ChatClient } from '../src/client/clients/chatClient';
-import { createChat, registerChat } from '../src/server/components/chat';
-import { Message } from '../src/shared/stores/chatStore';
+import { createChat, createChatWithCallback, registerChat } from '../src/server/components/chat';
+import { GameConstructor } from '../src/server/games';
+import { Action, Message } from '../src/shared/stores/chatStore';
 import { TestServer } from './helper';
+
+type SendAction<Action> = (action: Action) => Promise<void>;
+
+async function testChat<Action>(chatCreator: GameConstructor, cb: (chatClient: ChatClient, sendAction: SendAction<Action>) => Promise<void>) {
+  const server = new TestServer();
+  const client = server.createClient();
+
+  registerChat(server, true);
+
+  const gameId = 0;
+  server.addToGroup(client, `game/${gameId}`);
+  const chatServer = chatCreator(gameId);
+  const chatClient = new ChatClient({ client, gameId });
+  chatClient.initialize();
+  // TODO why is this needed?
+  await chatClient.getState();
+
+  const sendAction: SendAction<Action> = async action => {
+    return client.call('chat/action', gameId, action);
+  };
+  await cb(chatClient, sendAction);
+
+  chatServer(gameId);
+}
 
 describe('chat client', () => {
   it('client should be able to send a message', async () => {
-    const server = new TestServer();
-    const client = server.createClient();
+    await testChat(createChat(), async chatClient => {
+      await chatClient.sendMessage('test message');
 
-    registerChat(server, true);
-    const chatCreator = createChat();
+      const info = chatClient.client.getUserInfo();
+      const expectedMessage: Message = {
+        user: {
+          id: info?.id ?? '',
+          name: info?.name ?? '',
+        },
+        message: 'test message',
+      };
 
-    const gameId = 0;
-    server.addToGroup(client, `game/${gameId}`);
-    const chatServer = chatCreator(gameId);
-    const chatClient = new ChatClient({ client, gameId });
-    chatClient.initialize();
+      expect(chatClient.store.getState().messages).toEqual([expectedMessage]);
+    });
+  });
 
-    await chatClient.getState();
-    await chatClient.sendMessage('test message');
+  it('client should be able to send a message', async () => {
+    let messageReceived: Message | undefined;
+    await testChat(
+      createChatWithCallback((cb, user, message) => {
+        messageReceived = {
+          user,
+          message,
+        };
+      }),
+      async chatClient => {
+        await chatClient.sendMessage('test message');
 
-    const expectedMessage: Message = {
-      user: {
-        id: client.getUserInfo()?.id ?? '',
-        name: client.getUserInfo()?.name ?? '',
-      },
-      message: 'test message',
-    };
+        const info = chatClient.client.getUserInfo();
+        const expectedMessage: Message = {
+          user: {
+            id: info?.id ?? '',
+            name: info?.name ?? '',
+          },
+          message: 'test message',
+        };
 
-    expect(chatClient.store.getState().messages).toEqual([expectedMessage]);
+        expect(chatClient.store.getState().messages).toEqual([expectedMessage]);
+        expect(messageReceived).toEqual(expectedMessage);
+      }
+    );
+  });
 
-    chatServer(gameId);
+  // this shouldn't pass, but passes since we are disabling validation on the server
+  xit('client should be allowed to send a message as a different user', async () => {
+    await testChat<Action>(createChat(), async (_chatClient, sendAction) => {
+      await sendAction({
+        type: 'message',
+        message: {
+          user: {
+            id: 'otherUser',
+            name: 'otherUser',
+          },
+          message: 'test message',
+        },
+      });
+    });
   });
 });
