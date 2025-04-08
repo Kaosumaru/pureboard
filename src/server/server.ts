075aef5e0f1a2b019b9caceaed01197015734297
Mateusz Borycki <mateuszborycki@gmail.com>
2025-04-08 13:49:14 +0200

    feat(server): automatically register games, char and timer

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,12 @@
 import { RPCServer } from 'yawr';
+import { registerGames } from './games';
+import { registerChat } from './components/chat';
+import { registerTimer } from './components/timer';
 
 export function createServer(port?: number): RPCServer {
-  return new RPCServer(port);
+  const server = new RPCServer(port);
+  registerGames(server);
+  registerChat(server);
+  registerTimer(server);
+  return server;
 }
